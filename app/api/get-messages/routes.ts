058a0275facbf88090d2cc2da027455246cc5b22
Rahul-dev02 <rahulroa14320@gmail.com>
2026-08-05 06:42:38 +0530

    backend complete

--- app/api/get-messages/routes.ts
+++ app/api/get-messages/routes.ts
@@ -3,6 +3,7 @@ import { authOptions } from "../auth/[...nextauth]/option";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { User } from "next-auth";
+import mongoose from "mongoose";
 
 
 export async function POST(request:Request){
@@ -20,5 +21,41 @@ export async function POST(request:Request){
     })
    }
 
-   const userId= user._id
+   const userId= new mongoose.Types.ObjectId(user._id)
+   try {
+    const user= await UserModel.aggregate([
+        { $match: {id:userId}},
+        {$unwind :"$messages"},
+        {$sort:{'messages.createdAt':-1}},
+        {$group:{
+            _id:"$_id",
+            messages:{$push:"$messages"}}}
+    ])
+
+    if(!user || user.length ===0){
+        return Response.json({
+        success :false,
+        message:"User  not found"
+    },{
+        status:401 
+    })
+    }
+
+     return Response.json({
+        success :true,
+        messages:user[0].messages
+    },{
+        status:200 
+    }) 
+
+   } catch (error) {
+    console.log("An unexpeted Error occudred while fetching messages",error)
+     return Response.json({
+        success :false,
+        message:"An error occurred while fetching messages"
+    },{
+        status:500 
+    })
+   }
 }
+
